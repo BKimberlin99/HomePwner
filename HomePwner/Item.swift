b9c0285c0335c145b71f257e7b7b784efb165ad3
Brandon Kimberlin <bkimberlin@humantorch.local>
2019-03-18 13:43:12 -0400

    Deleted starting viewController and viewController.swift, created Item.swift & ItemsViewController.swift, added Table View Controller to Main.storyboard, finished page 181

--- HomePwner/Item.swift
+++ HomePwner/Item.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2019 Brandon Kimberlin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Item: NSObject {
+    var name: String
+    var valueInDollars: Int
+    var serialNumber: String?
+    var dateCreated: Date
+    
+    init(name: String, serialNumber: String?, valueInDollars: Int) {
+        self.name = name
+        self.valueInDollars = valueInDollars
+        self.serialNumber = serialNumber
+        self.dateCreated = Date()
+        
+        super.init()
+    }
+}
